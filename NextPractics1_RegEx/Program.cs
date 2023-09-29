10210a49a9bea82fcb18c71bbbf789264734fae3
wideprism <winsvintus@gmail.com>
2023-09-29 12:00:13 +0300

    Add project files.

---
+++ NextPractics1_RegEx/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NextPractics1_RegEx
+{
+    internal class Program
+    {
+        protected static string Replacement(string inputString, List<string> valuesToReplace, string replaceValue)
+        {
+            string regexValues = string.Join("|", valuesToReplace);
+            return Regex.Replace(inputString, regexValues, replaceValue);
+        }
+        static void Task1()
+        {
+            System.Console.InputEncoding = System.Text.Encoding.Unicode;
+            System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Введіть логін: ");
+            string? inputLogin = Console.ReadLine();
+            Regex loginLimits = new(@"^(\D)(\w){1,9}$");
+            switch (loginLimits.IsMatch(inputLogin))
+            {
+                case false:
+                    Console.WriteLine("На жаль, ваш логін порушує правила сервісу.");
+                    break;
+                case true:
+                    Console.WriteLine("Ласкаво просимо на нашому порталі!");
+                    break;
+
+            }
+
+        }
+        static void Task2()
+        {
+            System.Console.InputEncoding = System.Text.Encoding.Unicode;
+            System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Введіть довільний текст: ");
+            string? inputText = Console.ReadLine();
+            string? processingText = inputText;
+            string outputText = "";
+            List<string> patternsToReplaceUpper = new() {
+                                            "Будь-ласка",
+                                            "Будьласка",
+                                            "Буд ласка",
+                                            "Будь аска",
+                                            "Будь лска",
+                                            "Будь лака",
+                                            "Будь ласа",
+                                            "Будь ласк"
+                                            };
+            List<string> patternsToReplaceLower = new() {
+                                            "будь-ласка",
+                                            "будьласка",
+                                            "буд ласка",
+                                            "будь аска",
+                                            "будь лска",
+                                            "будь лака",
+                                            "будь ласа",
+                                            "будь ласк"
+                                            };
+            processingText = Replacement(processingText, patternsToReplaceLower, "будь ласка");
+            processingText = Replacement(processingText, patternsToReplaceUpper, "Будь ласка");
+            outputText = processingText;
+            Console.WriteLine("Відредагований текст: {0}", outputText);
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+        }
+    }
+}
---
+++ NextPractics1_RegEx/ProgramExample.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+//        //string data1 = "Петро, ​​Андрій, Микола";
+//        //string data2 = "Петро, ​​Андрій, Олександр";
+//        //Regex myReg = new Regex("Микола"); // Створення регулярного виразу
+//        //Console.WriteLine(myReg.IsMatch(data1)); // True
+//        //Console.WriteLine(myReg.IsMatch(data2)); // False
+//        //string data1 = "Петро, ​​Андрій, Микола" ;
+//        //Regex myReg = new("Микола");
+//        //Match match = myReg.Match(data1);
+//        //Console.WriteLine(match.Value); // "Микола"
+//        //Console.WriteLine(match.Index); // 15
+//        //string data1 = "Петро, Микола, Андрій, Микола";
+//        //Regex myReg = new Regex("Микола");
+//        //data1 = myReg.Replace(data1, "Максим");
+//        //Console.WriteLine(data1); // "Петро, ​​Максим, Андрій, Максим"
+//        //Regex myReg = new Regex(", ");
+//        //string[] names = myReg.Split(data1); // Масив імен
+//        //foreach (string element in names)
+//        //{
+//        //    Console.WriteLine(element);
+//        //}
+//        //string data = "nikolay, sergey, oleg" ;
+//        //Regex myRegIgnoreCase = new Regex(@"Sergey", RegexOptions.IgnoreCase);
+//        //Regex myReg = new Regex(@"Sergey");
+//        //Console.WriteLine(myRegIgnoreCase.IsMatch(data)); // True
+//        //Console.WriteLine(myReg.IsMatch(data)); // False
+
+//    }
+//}
+
+//class Program
+//{
+//    static string GetDomain(string url)
+//    {
+//        Regex re = new Regex("https://", RegexOptions.IgnoreCase);
+//        url = re.Replace(url, ""); // Видаляємо частину http: //
+//        Regex reWww = new Regex(@"www\\.", RegexOptions.IgnoreCase);
+//        url = reWww.Replace(url, ""); // Видаляємо частину www.
+//        int end = url.IndexOf("/");
+
+//        if (end != -1)
+//            url = url.Substring(0, end);
+//        return url;
+//    }
+//    static void Main(string[] args)
+//    {
+
+//        string url1 = "https://krypton.com.ua/tutorial/csharp/";
+//        string url2 = "https://krypton.com.ua/tutorial/ci-10-net-6/";
+//        Console.WriteLine(GetDomain(url1)); // krypton.com.ua
+//        Console.WriteLine(GetDomain(url2)); // krypton.com.ua
+//    }
+//}
